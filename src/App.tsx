f9f19edd01addb0b24bbbdb14b53ea9043a50e8b
David Fredkove <dave.fredkove@gmail.com>
2025-11-13 11:18:38 -0600

    simplified layout and added some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import Home from "./components/Home"
+// import HomeHero from "./components/Hero"
 
 export default function PortfolioLayout() {
   const [menuOpen, setMenuOpen] = useState(false)
 
   return (
-    <div className="min-h-screen bg-main pt-12">
-      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Home />
+    <div className="flex flex-col min-h-screen bg-main">
+      {/* <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home /> */}
+      <div className="bg-nav text-primary">
+        <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+      <div className="text-primary">
+        <Home />
+        {/* <HomeHero /> */}
+      </div>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react"
 import Dark from "../assets/dark.svg"
 import Light from "../assets/light.svg"
+import Logo from "../assets/logo.png"
 
 type Props = {
   menuOpen: boolean
   setMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
-
 export default function Header({ menuOpen, setMenuOpen }: Props) {
   const [theme, setTheme] = useState("dark")
 
@@ -23,101 +23,108 @@ export default function Header({ menuOpen, setMenuOpen }: Props) {
   }, [])
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 h-12 w-full bg-nav shadow-md text-primary">
-      <div className="mx-auto flex h-full w-full items-center justify-between px-4 lg:max-w-7xl">
-        <a
-          href="#"
-          className="font-medium text-xl tracking-wide flex items-center gap-2"
-        >
-          <span className="text-4xl">🦸‍♂️</span>
-          <span>ExpertDave</span>
-        </a>
-
-        {/* Desktop nav */}
-        <nav className="hidden gap-8 text-md sm:flex">
-          <span onClick={toggleTheme}>
-            <img
-              src={theme === "dark" ? Light : Dark}
-              alt="theme icon"
-              className="h-6 w-6 cursor-pointer"
-            />
-          </span>
-          <a href="#projects" className="hover:underline">
-            Projects
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-nav shadow-md">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-12 sm:h-16 text-primary">
+          <a href="#" className="flex items-center gap-2">
+            <img src={Logo} alt="Logo" className="size-8 sm:size-12" />
+            <span className="font-medium text-lg sm:text-4xl tracking-wider">
+              ExpertDave
+            </span>
           </a>
-          <a href="#about" className="hover:underline">
-            About
-          </a>
-          <a href="#contact" className="hover:underline">
-            Contact
-          </a>
-        </nav>
-
-        {/* Hamburger icon (visible on small screens) */}
-        <button
-          className="sm:hidden text-primary focus:outline-none cursor-pointer"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          {menuOpen ? (
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
+          <ul className="hidden sm:flex space-x-4">
+            <span onClick={toggleTheme}>
+              <img
+                src={theme === "dark" ? Light : Dark}
+                alt="theme icon"
+                className="h-6 w-6 cursor-pointer"
               />
-            </svg>
-          ) : (
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+            </span>
+            <li>
+              <a className="hover:underline" href="#">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="hover:underline" href="#">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="hover:underline" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          {/* Hamburger icon (visible on small screens) */}
+          <button
+            className="sm:hidden text-primary focus:outline-none cursor-pointer"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            {menuOpen ? (
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </button>
+
+          {/* Mobile dropdown */}
+          {menuOpen && (
+            <div className="absolute left-0 top-12 w-full bg-nav shadow-md sm:hidden">
+              <nav className="flex flex-col space-y-2 gap-4 px-4 py-6 text-xl text-primary text-center">
+                <a
+                  href="#projects"
+                  onClick={() => setMenuOpen(false)}
+                  className="hover:underline"
+                >
+                  Projects
+                </a>
+                <a
+                  href="#about"
+                  onClick={() => setMenuOpen(false)}
+                  className="hover:underline"
+                >
+                  About
+                </a>
+                <a
+                  href="#contact"
+                  onClick={() => setMenuOpen(false)}
+                  className="hover:underline"
+                >
+                  Contact
+                </a>
+              </nav>
+            </div>
           )}
-        </button>
-      </div>
 
-      {/* Mobile dropdown */}
-      {menuOpen && (
-        <div className="absolute left-0 top-12 w-full bg-nav shadow-md sm:hidden">
-          <nav className="flex flex-col space-y-2 gap-4 px-4 py-6 text-xl text-primary text-center">
-            <a
-              href="#projects"
-              onClick={() => setMenuOpen(false)}
-              className="hover:underline"
-            >
-              Projects
-            </a>
-            <a
-              href="#about"
-              onClick={() => setMenuOpen(false)}
-              className="hover:underline"
-            >
-              About
-            </a>
-            <a
-              href="#contact"
-              onClick={() => setMenuOpen(false)}
-              className="hover:underline"
-            >
-              Contact
-            </a>
-          </nav>
+          {/* asdf */}
         </div>
-      )}
-    </header>
+      </div>
+    </nav>
   )
 }
